8994dab454a0d465e59ae93936c9a1179b131f69
cj <seejarom@gmail.com>
2021-09-13 23:32:55 -0600

    starting on profiles, front end

--- keepr.client/src/pages/ProfilePage.vue
+++ keepr.client/src/pages/ProfilePage.vue
@@ -5,7 +5,29 @@
 </template>
 
 <script>
+import { computed, onMounted, ref } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+import Pop from '../utils/Notifier'
+import { accountService } from '../services/AccountService'
+import { route, useRoute } from "vue-router"
 export default {
-  name: 'ProfilePage'
+  name: 'ProfilePage',
+  const route: useRoute,
+  setup() {
+    const route: useRoute,
+    const loading = ref(true)
+    onMounted(async() => {
+      try {
+        await accountService.getAccount(route.params.id)
+        loading.value = false
+      } catch (error) {
+        Pop.toast(error, 'error')
+      }
+    })
+    return {
+      account: computed(() => AppState.account)
+    }
+  },
+  components: {}
 }
 </script>
